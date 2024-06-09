4612e8d275844aa8f8b98030a58265a73eb63104
zhulinwei <1171567018@qq.com>
2024-06-09 20:55:47 +0800

    update

--- api/rollout_sample/v1/doc.go
+++ api/rollout_sample/v1/doc.go
@@ -14,6 +14,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
+// +genclient
 // +groupName=zlw.com
 // +kubebuilder:object:generate=true
 package v1
