8ac8d03294afb0421b788179903e4339930a73f3
zhulinwei <1171567018@qq.com>
2024-06-14 22:48:23 +0800

    update

--- sample/dynamic/main.go
+++ sample/dynamic/main.go
@@ -17,22 +17,26 @@ func init() {
 }
 
 func main() {
+	// 从kubeconfig中获取k8s配置
 	restConfig, err := clientcmd.BuildConfigFromFlags("", clientcmd.RecommendedHomeFile)
 	if err != nil {
 		return
 	}
 
+	// 初始化dynamicClient客户端
 	dynamicClient, err := dynamic.NewForConfig(restConfig)
 	if err != nil {
 		return
 	}
 
+	// 使用dynamicClient获取unstructured对象
 	obj, err := dynamicClient.Resource(samplev1.SchemeGroupVersionResource).Namespace("default").List(context.Background(), v1.ListOptions{})
 	if err != nil {
 		println(err.Error())
 		return
 	}
 
+	// 将unstructured对象转化为Go结构体
 	var list = &samplev1.RolloutSampleList{}
 	err = runtime.DefaultUnstructuredConverter.FromUnstructured(obj.UnstructuredContent(), &list)
 	if err != nil {
